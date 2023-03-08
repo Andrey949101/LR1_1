a6d1621e51ca14799793b396b2ad062ec1c392da
Andrey9491 <belandr9491.spb@gmail.com>
2023-03-08 11:34:07 +0200

    LR2_2

--- scripts/listener2.py
+++ scripts/listener2.py
@@ -5,6 +5,6 @@ from std_msgs.msg import String
 def callback(msg):
     rospy.loginfo("I heard %s", msg.data)
 
-rospy.init_node('listener_chisla')
-rospy.Subscriber('Chisla_do_100', String, callback, queue_size=10)
+rospy.init_node('listener_sotni')
+rospy.Subscriber('Sotni', String, callback, queue_size=10)
 rospy.spin()
--- scripts/talker.py
+++ scripts/talker.py
@@ -2,18 +2,32 @@
 import rospy
 from std_msgs.msg import String
 
-rospy.init_node('talker')
-pub = rospy.Publisher('my_chat_topic', String, queue_size=10)
+rospy.init_node('talker_chisla')
+pub1 = rospy.Publisher('Chisla_do_100', String, queue_size=10)
+pub2 = rospy.Publisher('Sotni', String, queue_size=10)
 rate = rospy.Rate(1)
+
 
 def start_talker():
     msg = String()
+    chisla = 0
+    sotni = 1
     while not rospy.is_shutdown():
-        hello_str = "hi =) %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
+        hello_str_1 = str(chisla) 
+        hello_str_2 = str(sotni) 
 
-        msg.data = hello_str
-        pub.publish(msg)
+        if chisla<100:
+            msg.data = hello_str_1
+            pub1.publish(msg)
+            rospy.loginfo(hello_str_1)
+            chisla=chisla+10
+        else: 
+            msg.data = hello_str_2
+            pub2.publish(msg)
+            rospy.loginfo(hello_str_2)
+            sotni=sotni+1
+            chisla=0
+
 
         rate.sleep()
 
